081fdc8dec113d23a3263d6d13764752de393539
Mohammad Sultan Al Nahiyan <nahiyan762@users.noreply.github.com>
2021-11-21 15:28:10 +0600

    init commit

--- image/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module image {
-    exports org.example.image to org.example.security, security;
-    requires java.desktop;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.core;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.services.rekognition;
-    requires org.slf4j;
-}
--- image/src/main/java/org/example/image/ImageService.java
+++
@@ -1,7 +0,0 @@
-package org.example.image;
-
-import java.awt.image.BufferedImage;
-
-public interface ImageService {
-    boolean imageContainsCat(BufferedImage image, float confidenceThreshhold);
-}
